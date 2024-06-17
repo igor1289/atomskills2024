5fcc6830d0b97a4e6f115e94a6f5a54c4ac6dca0
Елена <elena@example.com>
2024-06-17 18:50:16 +0500

    добавлена таблица результатов

---
+++ app/models/resalt.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../common/sequelize");
+
+class Resalt extends Model {}
+
+Resalt.init({
+  id: {
+    type: DataTypes.CHAR(32),
+    primaryKey: true,
+    allowNull: false,
+  },
+  task_code: {
+    type: DataTypes.CHAR(32),
+    allowNull: false,
+  },
+  coment: {
+    type: DataTypes.STRING(512),
+    allowNull: false,
+  },
+  student_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  teacher_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  score_id: {
+    type: DataTypes.CHAR(32),
+    allowNull: false,
+  },
+  status_id: {
+    type: DataTypes.CHAR(32),
+    allowNull: false,
+  },
+});
+
+module.exports = Resalt;
