e60f0ab1f86d543386dd49cdecf49d7289f55e3d
Елена <elena@example.com>
2024-06-17 11:59:12 +0500

    таблица пользователей

--- app/models/user.js
+++ app/models/user.js
@@ -2,59 +2,70 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../common/sequelize");
 const bcrypt = require("bcrypt");
 
-class User extends Model
-{
-    async setPassword(password)
-    {
-        try {
-            this.password = await bcrypt.hash(password, parseInt(process.env.SALT_ROUNDS));
-            console.log(this.password);
-        } catch (error) {
-            console.log(error);
-        }
+class User extends Model {
+  async setPassword(password) {
+    try {
+      this.password = await bcrypt.hash(
+        password,
+        parseInt(process.env.SALT_ROUNDS)
+      );
+      console.log(this.password);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    async verifyPassword(password)
-    {
-        try {
-            return bcrypt.compare(password, this.password); 
-        } catch (error) {
-            console.log(error);
-        }
+  async verifyPassword(password) {
+    try {
+      return bcrypt.compare(password, this.password);
+    } catch (error) {
+      console.log(error);
     }
+  }
 
-    getPayload()
-    {
-        return {
-            id: this.id,
-            name: this.name
-        }
-    }
+  getPayload() {
+    return {
+      id: this.id,
+      name: this.name,
+    };
+  }
 }
 
-User.init({
+User.init(
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
     },
     name: {
-        type: DataTypes.STRING(256),
-        allowNull: false,
+      type: DataTypes.STRING(256),
+      allowNull: false,
     },
     password: {
-        type: DataTypes.STRING(256),
-        allowNull: false
+      type: DataTypes.STRING(256),
+      allowNull: false,
     },
     isAdmin: {
-        type: DataTypes.BOOLEAN,
-        allowNull: false,
-        defaultValue: false
-    }
-},
-{
-    sequelize
-})
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    isTeacher: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+    firstName: { type: DataTypes.STRING(256), allowNull: false },
+    lastName: { type: DataTypes.STRING(256), allowNull: false },
+    fatherName: { type: DataTypes.STRING(256), allowNull: false },
+    email: { type: DataTypes.STRING(256), allowNull: false },
+    tel: { type: DataTypes.STRING(256), allowNull: false },
+  },
+  {
+    sequelize,
+  }
+);
 
-module.exports = User
+module.exports = User;
