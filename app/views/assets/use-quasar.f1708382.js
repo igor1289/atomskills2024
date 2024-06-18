251850a6e05fb9043a84c058ef169aae034afc93
ANNA <ann@example.com>
2024-06-18 16:10:53 +0500

    fix

--- app/views/assets/use-quasar.f1708382.js
+++
@@ -1,1 +0,0 @@
-import{B as a,a1 as r}from"./index.4e3e9fa9.js";function u(){return a(r)}export{u};
--- src/pages/q-rating.vue
+++ src/pages/q-rating.vue
@@ -1,34 +1,16 @@
 <template>
-  <div>
-    <apexchart
-      width="500"
-      type="bar"
-      :options="chartOptions"
-      :series="series"
-    ></apexchart>
-  </div>
+  <q-page class="flex flex-center">
+    <h1>tasks</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam qui,
+      porro optio pariatur rerum doloribus alias dicta est accusantium adipisci
+      id repellat quidem iste fugit vero voluptatem ipsam quis doloremque.
+    </p>
+  </q-page>
 </template>
 
-<script>
-import VueApexCharts from "vue3-apexcharts";
-export default {
-  data: function () {
-    return {
-      chartOptions: {
-        chart: {
-          id: "vuechart-example",
-        },
-        xaxis: {
-          categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
-        },
-      },
-      series: [
-        {
-          name: "series-1",
-          data: [30, 40, 35, 50, 49, 60, 70, 91],
-        },
-      ],
-    };
-  },
-};
+<script setup>
+defineOptions({
+  name: "IndexPage",
+});
 </script>
