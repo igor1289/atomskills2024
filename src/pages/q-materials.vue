260300106b449e76bfea2af995405eac5465c023
ANNA <ann@example.com>
2024-06-18 13:41:24 +0500

    дерево уроков и форма открытия урока

--- src/pages/q-materials.vue
+++ src/pages/q-materials.vue
@@ -1,232 +1,177 @@
 <template>
-  <div class="q-pa-md">
-    <q-table
-      class="my-sticky-virtscroll-table shadow-2"
-      color="white"
-      flat
-      bordered
-      ref="tableMat"
-      :rows="rows"
-      :columns="columns"
-      row-key="id"
-      v-model:pagination="pagination"
-      :loading="loading"
-      :filter="filter"
-      binary-state-sort
-      @request="onRequest"
-    >
-      <template v-slot:top-right>
-        <q-input
-          borderless
-          dense
-          debounce="300"
-          v-model="filter"
-          placeholder="Поиск"
-        >
-          <template v-slot:append>
-            <q-icon name="search" />
-          </template>
-        </q-input>
+  <div>
+    <q-splitter v-model="splitterModel" style="height: 900px">
+      <template v-slot:before>
+        <div class="q-pa-md">
+          <q-input ref="filterRef" filled v-model="filter" label="Поиск">
+            <template v-slot:append>
+              <q-icon
+                v-if="filter !== ''"
+                name="clear"
+                class="cursor-pointer"
+                @click="resetFilter"
+              />
+            </template>
+          </q-input>
+          <q-tree
+            :nodes="simple"
+            :filter="filter"
+            node-key="label"
+            selected-color="primary"
+            v-model:selected="selected"
+            default-expand-all
+          />
+        </div>
       </template>
-    </q-table>
+
+      <template v-slot:after>
+        <q-tab-panels
+          v-model="selected"
+          animated
+          transition-prev="jump-up"
+          transition-next="jump-up"
+        >
+          <q-tab-panel name="Учебный матреиал 1">
+            <div class="text-h4 q-mb-md text-center">Учебный матреиал 1</div>
+            <div class="text-h6 q-mb-md text-right">Автор</div>
+            <p class="text-justify">
+              ОПИСАНИЕ Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Quis praesentium cumque magnam odio iure quidem, quod illum
+              numquam possimus obcaecati commodi minima assumenda consectetur
+              culpa fuga nulla ullam. In, libero.
+            </p>
+            <div>
+              <q-table
+                class="my-sticky-header-column-table"
+                bordered
+                title="Список приложений"
+                :rows="rows"
+                :columns="columns"
+                row-key="name"
+              >
+                <template v-slot:body-cell-image="props">
+                  <q-td :props="props">
+                    <div>
+                      <img :src="props.row.image" />
+                    </div>
+                  </q-td>
+                </template>
+              </q-table>
+            </div>
+            <div class="q-pt-md">
+              <q-table
+                title="Задания"
+                bordered
+                :rows="rowsTasks"
+                :columns="columnsTasks"
+                row-key="nameKey"
+              >
+              </q-table>
+            </div>
+          </q-tab-panel>
+        </q-tab-panels>
+      </template>
+    </q-splitter>
   </div>
 </template>
 
 <script>
-import { ref, onMounted } from "vue";
-
+import { ref } from "vue";
 const columns = [
   {
     name: "title",
     required: true,
-    label: "Название",
+    label: "Заголовок",
     align: "left",
     field: (row) => row.name,
-    format: (val) => `${val}`,
-    sortable: true,
-  },
-
-  {
-    name: "difficulty",
-    label: "Сложность",
-    field: "difficulty",
     sortable: true,
   },
   {
-    name: "time",
-    label: "Время выполнения (мин)",
-    field: "time",
+    name: "image",
+    label: "Файл",
+    field: "image",
+    sortable: false,
+  },
+];
+const columnsTasks = [
+  {
+    name: "tasks",
+    required: true,
+    label: "Задание",
+    align: "left",
+    field: (row) => row.name,
     sortable: true,
+  },
+  {
+    name: "code",
+    label: "Code",
+    sortable: false,
+  },
+];
+const rowsTasks = [
+  {
+    name: "Task1",
+    code: 123,
+  },
+  {
+    name: "Task2",
+    code: 321,
   },
 ];
 
-const originalRows = [
+const rows = [
   {
-    id: 1,
-    name: "Задание1",
-    difficulty: 1,
-    time: 14,
+    name: "Картинка1",
+    image: "https://via.placeholder.com/150",
   },
   {
-    id: 2,
-    name: "Задание2",
-    difficulty: 3,
-    time: 10,
+    name: "Картинка2",
+    image: "https://via.placeholder.com/150",
   },
   {
-    id: 3,
-    name: "Задание3",
-    difficulty: 1,
-    time: 34,
-  },
-  {
-    id: 4,
-    name: "Задание4",
-    difficulty: 2,
-    time: 16,
-  },
-  {
-    id: 5,
-    name: "Задание5",
-    difficulty: 3,
-    time: 5,
+    name: "Картинка3",
+    image: "https://via.placeholder.com/150",
   },
 ];
 
 export default {
   setup() {
-    const tableMat = ref();
-    const rows = ref([]);
     const filter = ref("");
-    const loading = ref(false);
-    const pagination = ref({
-      sortBy: "desc",
-      descending: false,
-      page: 1,
-      rowsPerPage: 10,
-      rowsNumber: 10,
-    });
-
-    // emulate ajax call
-    // SELECT * FROM ... WHERE...LIMIT...
-    function fetchFromServer(startRow, count, filter, sortBy, descending) {
-      const data = filter
-        ? originalRows.filter((row) => row.name.includes(filter))
-        : originalRows.slice();
-
-      // handle sortBy
-      if (sortBy) {
-        const sortFn =
-          sortBy === "desc"
-            ? descending
-              ? (a, b) => (a.name > b.name ? -1 : a.name < b.name ? 1 : 0)
-              : (a, b) => (a.name > b.name ? 1 : a.name < b.name ? -1 : 0)
-            : descending
-            ? (a, b) => parseFloat(b[sortBy]) - parseFloat(a[sortBy])
-            : (a, b) => parseFloat(a[sortBy]) - parseFloat(b[sortBy]);
-        data.sort(sortFn);
-      }
-
-      return data.slice(startRow, startRow + count);
-    }
-
-    // emulate 'SELECT count(*) FROM ...WHERE...'
-    function getRowsNumberCount(filter) {
-      if (!filter) {
-        return originalRows.length;
-      }
-      let count = 0;
-      originalRows.forEach((treat) => {
-        if (treat.name.includes(filter)) {
-          ++count;
-        }
-      });
-      return count;
-    }
-
-    function onRequest(props) {
-      const { page, rowsPerPage, sortBy, descending } = props.pagination;
-      const filter = props.filter;
-
-      loading.value = true;
-
-      // emulate server
-      setTimeout(() => {
-        // update rowsCount with appropriate value
-        pagination.value.rowsNumber = getRowsNumberCount(filter);
-
-        // get all rows if "All" (0) is selected
-        const fetchCount =
-          rowsPerPage === 0 ? pagination.value.rowsNumber : rowsPerPage;
-
-        // calculate starting row of data
-        const startRow = (page - 1) * rowsPerPage;
-
-        // fetch data from "server"
-        const returnedData = fetchFromServer(
-          startRow,
-          fetchCount,
-          filter,
-          sortBy,
-          descending
-        );
-
-        // clear out existing data and add new
-        rows.value.splice(0, rows.value.length, ...returnedData);
-
-        // don't forget to update local pagination object
-        pagination.value.page = page;
-        pagination.value.rowsPerPage = rowsPerPage;
-        pagination.value.sortBy = sortBy;
-        pagination.value.descending = descending;
-
-        // ...and turn of loading indicator
-        loading.value = false;
-      }, 1500);
-    }
-
-    onMounted(() => {
-      // get initial data from server (1st page)
-      tableMat.value.requestServerInteraction();
-    });
-
+    const filterRef = ref(null);
     return {
-      tableMat,
+      splitterModel: ref(30),
+      selected: ref("Food"),
       filter,
-      loading,
-      pagination,
+      filterRef,
       columns,
       rows,
+      columnsTasks,
+      rowsTasks,
 
-      onRequest,
+      simple: [
+        {
+          label: "Тема1",
+          children: [
+            {
+              label: "Учебный матреиал 1",
+              icon: "restaurant_menu",
+            },
+            {
+              label: "Учебный матреиал 2",
+              icon: "room_service",
+            },
+            {
+              label: "Учебный матреиал 3",
+              icon: "photo",
+            },
+          ],
+        },
+      ],
+      resetFilter() {
+        filter.value = "";
+        filterRef.value.focus();
+      },
     };
   },
 };
 </script>
-
-<style lang="sass">
-.my-sticky-virtscroll-table
-  /* height or max-height is important */
-  max-height: 850px
-
-  .q-table__top,
-  .q-table__bottom,
-  thead tr:first-child th /* bg color is important for th; just specify one */
-    background-color: $primary
-    color: white
-
-  thead tr th
-    position: sticky
-    z-index: 1
-  /* this will be the loading indicator */
-  thead tr:last-child th
-    /* height of all previous header rows */
-    top: 48px
-  thead tr:first-child th
-    top: 0
-
-  /* prevent scrolling behind sticky top row on focus */
-  tbody
-    /* height of all previous header rows */
-    scroll-margin-top: 48px
-</style>
